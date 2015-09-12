4b4cd47a3533f307549fd6211879363d157e7f08
kitakamiooi <kitakami@ooi.moe>
2015-09-12 15:31:03 +0800

    加入了多个命令行参数，并提供了多进程支持

--- config.py
+++ config.py
@@ -1,17 +1,14 @@
 import os
 
-# Tornado directories
+# Tornado Settings
 base_dir = os.path.abspath(os.path.dirname(__file__))
 template_path = os.path.join(base_dir, 'templates')
 static_path = os.path.join(base_dir, 'static')
-
-# Tornado Settings
 cookie_secret = os.environ.get('OOI_SECRET', 'DEFAULT COOKIE SECRET FOR DEVELOPING')
-debug = bool(os.environ.get('OOI_DEBUG', False))
 
 # Proxy settings
-proxy_host = os.environ.get('OOI_AUTH_PROXY_HOST', None)
-proxy_port = int(os.environ.get('OOI_AUTH_PROXY_PORT')) if os.environ.get('OOI_AUTH_PROXY_PORT') else None
+proxy_host = os.environ.get('OOI_PROXY_HOST', None)
+proxy_port = int(os.environ.get('OOI_PROXY_PORT')) if os.environ.get('OOI_AUTH_PROXY_PORT') else None
 
 # Customized settings
 customize_dir = os.path.join(base_dir, 'customize')
--- ooi.py
+++ ooi.py
@@ -1,32 +1,41 @@
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
-from tornado.options import define, options
+from tornado.options import define, options, parse_command_line
 from ooi.handlers import MainHandler, NormalGameHandler, iFrameGameHandler, iFrameFlashHandler, \
     PoiGameHandler, ReloginHandler
 from api.handlers import ApiHandler, MainSwfHandler, WorldImageHandler
-from config import template_path, static_path, cookie_secret, debug
+from config import template_path, static_path, cookie_secret
 from ui import modules
 
-application = tornado.web.Application(
-    handlers=[('/', MainHandler),
-              ('/kancolle', NormalGameHandler),
-              ('/iframe', iFrameGameHandler),
-              ('/flash', iFrameFlashHandler),
-              ('/poi', PoiGameHandler),
-              (r'/kcsapi/(.*)', ApiHandler),
-              ('/kcs/mainD2.swf', MainSwfHandler),
-              (r'/kcs/resources/image/world/.*(l|s)\.png', WorldImageHandler),
-              ('/relogin', ReloginHandler), ],
-    template_path=template_path,
-    static_path=static_path,
-    cookie_secret=cookie_secret,
-    ui_modules=modules,
-    debug=debug
-)
-
 define('port', type=int, default=8000)
+define('mp', type=bool, default=False)
+define('debug', type=bool, default=False)
 
 if __name__ == "__main__":
-    application.listen(options.port)
-    tornado.ioloop.IOLoop.current().start()
+    parse_command_line()
+    application = tornado.web.Application(
+        handlers=[('/', MainHandler),
+                  ('/kancolle', NormalGameHandler),
+                  ('/iframe', iFrameGameHandler),
+                  ('/flash', iFrameFlashHandler),
+                  ('/poi', PoiGameHandler),
+                  (r'/kcsapi/(.*)', ApiHandler),
+                  ('/kcs/mainD2.swf', MainSwfHandler),
+                  (r'/kcs/resources/image/world/.*(l|s)\.png', WorldImageHandler),
+                  ('/relogin', ReloginHandler), ],
+        template_path=template_path,
+        static_path=static_path,
+        cookie_secret=cookie_secret,
+        ui_modules=modules,
+        debug=options.debug
+    )
+
+    if options.debug or not options.mp:
+        application.listen(options.port)
+        tornado.ioloop.IOLoop.current().start()
+    else:
+        server = tornado.httpserver.HTTPServer(application)
+        server.bind(options.port)
+        server.start(0)
+        tornado.ioloop.IOLoop.current().start()
